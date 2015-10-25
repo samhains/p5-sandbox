7cc7483dd4c7ed37d696d13b0912b3f0545861e5
Sam Hains <sdh@eml.cc>
2015-10-25 12:37:38 +1100

    added height randomness

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,19 +1,26 @@
+BAR_HEIGHT = 1;
+CELL_WIDTH_MIN = 1
+CELL_WIDTH_MAX = 20
+CELL_HEIGHT_MIN = 1
+CELL_HEIGHT_MAX = 20
+CELL_SPEED_MIN = 0
+CELL_SPEED_MAX = 50
+
+var y = 100;
+var x = 200
+
+var cellX = { }
+var cellWidth = { }
+var cellHeight = { }
+var cellSpeed = { }
+var cellId = 0;
+
 function setup() {
   // uncomment this line to make the canvas the full size of the window
-   createCanvas(500, 300);
+   createCanvas(windowWidth, 400);
    strokeWeight(1);
    frameRate(30);
 }
-var y = 100;
-var x = 200
-var barHeight = 6;
-
-var cellX = {
-}
-var cellSpeed = {
-}
-var cellId = 0;
-
 // The statements in the setup() function
 // execute once when the program begins
 // program is stopped. Each statement is executed in
@@ -21,12 +28,12 @@ var cellId = 0;
 // line is executed again.
 
 function randomBarNumber(){
-  return barHeight*(random(height/barHeight));
+  return BAR_HEIGHT*(random(height/BAR_HEIGHT));
 }
 
 
 function randomBarLineArray(){
-  numOfBars = Math.floor(height/barHeight);
+  numOfBars = Math.floor(height/BAR_HEIGHT);
   var arr = [];
   for(var i=0; i< 10; i++){
     arr.push(random(numOfBars));
@@ -37,38 +44,42 @@ function createCell(id, startAt, barNum, speed){
   if (!cellX[id]){
     cellX[id] = startAt;
   }
+  if (!cellHeight[id]){
+    cellHeight[id] = BAR_HEIGHT*random(CELL_HEIGHT_MIN, CELL_HEIGHT_MAX);
+  }
+  if (!cellWidth[id]){
+    cellWidth[id] = BAR_HEIGHT*random(CELL_WIDTH_MIN, CELL_WIDTH_MAX);
+  }
   if (cellX[id] >= width){
-    cellX[id] = 0;
-    cellSpeed[id] = false;
+    resetCell(cellX, id)
   }
   if (!cellSpeed[id]){
     cellSpeed[id] = speed
   }
   fill('black')
   // X position, Y position, width, height
-  rect(cellX[id], barHeight*barNum, barHeight*3, barHeight);
+  rect(cellX[id], BAR_HEIGHT*barNum, cellWidth[id], cellHeight[id]);
   cellX[id] = cellX[id] + cellSpeed[id];
 
 }
 
-function createLines(){
-  numOfBars = height/barHeight
+function resetCell(cellX, id){
+  cellX[id] = 0;
+  cellSpeed[id] = false;
+  cellWidth[id] = random(CELL_WIDTH_MIN, CELL_WIDTH_MAX);
+}
+
+function createLines(color){
+  numOfBars = height/BAR_HEIGHT
   for(var i =0;i<numOfBars; i++){
-    line(0, i*barHeight, width, i*barHeight)
+    fill(color);
+    line(0, i*BAR_HEIGHT, width, i*BAR_HEIGHT)
   }
 }
 function draw() {
   background(255);
   fill('black');
-  //var n = noise(x) * width;
-  createLines();
-  for(var i = 0; i< height/barHeight; i++){
-    createCell(i, 0, i, random(100))
+  for(var i = 0; i< height/BAR_HEIGHT; i++){
+    createCell(i, 0, i, random(CELL_SPEED_MIN, CELL_SPEED_MAX))
   }
-  //createCell(0, 0, 0, 1)
-  //createCell(1, 0, 1, 2)
-  //createCell(2, 0, 2, 0.5)
-  //createCell(3, 0, 3, 1)
-  //createCell(4, 0, 4, 2)
-  //createCell(5, 0, 5, 10)
 }
