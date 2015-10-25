800a6a5ff4c2f84a0c7df4d5e6711cc08f5173af
Sam Hains <sdh@eml.cc>
2015-10-25 14:09:48 +1100

    added variable directions

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,10 +1,21 @@
+LINES_ON = false;
+
+BACKGROUND_COLOR = 'white'
+LINE_COLOR = 230
+
 BAR_HEIGHT = 10;
+
+JITTER_ON = false;
+NUM_OF_JITTERS = 40;
+JITTER_LINE_MIN = 1;
+JITTER_LINE_MAX = 4;
+
 CELL_WIDTH_MIN = 1
 CELL_WIDTH_MAX = 1
 CELL_HEIGHT_MIN = 1
 CELL_HEIGHT_MAX = 1
 CELL_SPEED_MIN = 1
-CELL_SPEED_MAX = 80
+CELL_SPEED_MAX = 4
 
 var y = 100;
 var x = 200
@@ -14,6 +25,7 @@ var cellWidth = {}
 var cellColor = {}
 var cellHeight = {}
 var cellSpeed = {}
+var reverseRow = {}
 var cellId = 0;
 
 function setup() {
@@ -51,11 +63,11 @@ function createCell(id, startAt, barNum, speed, color){
   if (!cellWidth[id]){
     cellWidth[id] = BAR_HEIGHT*random(CELL_WIDTH_MIN, CELL_WIDTH_MAX);
   }
-  if (!cellColor[id]){
-    cellColor[id] = color;
-  }
   if (cellX[id] >= width){
     resetCell(cellX, id)
+  }
+  if (!cellColor[id]){
+    cellColor[id] = color;
   }
   if (!cellSpeed[id]){
     cellSpeed[id] = speed/(cellWidth[id]/4);
@@ -69,24 +81,51 @@ function createCell(id, startAt, barNum, speed, color){
 }
 
 function resetCell(cellX, id){
-  cellX[id] = 0;
   cellSpeed[id] = false;
+  cellColor[id] = false;
   cellWidth[id] = BAR_HEIGHT*random(CELL_WIDTH_MIN, CELL_WIDTH_MAX);
+  cellX[id] = 0- cellWidth[id];
 }
 
 function createLines(color){
-  numOfBars = height/BAR_HEIGHT
-  for(var i =0;i<numOfBars; i++){
-    stroke(color);
-    line(0, i*BAR_HEIGHT, width, i*BAR_HEIGHT)
+  if(LINES_ON){
+    numOfBars = height/BAR_HEIGHT
+    for(var i =0;i<numOfBars; i++){
+      stroke(color);
+      line(0, i*BAR_HEIGHT, width, i*BAR_HEIGHT)
+    }
   }
 }
+function createJitterLines(currentLine){
+  if (JITTER_ON){
+    for(var j =0; j < random(JITTER_LINE_MIN, JITTER_LINE_MAX); j++){
+      createCell(currentLine+' '+j, random(width), currentLine, random(CELL_SPEED_MIN, CELL_SPEED_MAX), color(298));
+    }
+  }
+}
+function calculateSpeed(min, max, i){
+  var speed = random(CELL_SPEED_MIN, CELL_SPEED_MAX);
+
+  if (!reverseRow[i]){
+    reverseRow[i] = Math.floor(random(1,3));
+  }
+  if (reverseRow[i] == 2){
+    speed = -speed
+  }
+  return speed;
+}
+
 function draw() {
-  background(255);
-  createLines(200)
+  background(BACKGROUND_COLOR);
+  createLines(LINE_COLOR)
   for(var i = 0; i< height/BAR_HEIGHT; i++){
-    createCell(i,random(width) , i, random(CELL_SPEED_MIN, CELL_SPEED_MAX), color([random(200)]))
-    createCell(i+'b',random(width) , i, random(CELL_SPEED_MIN, CELL_SPEED_MAX), color([random(200)]))
-    createCell(i+'c',random(width) , i, random(CELL_SPEED_MIN, CELL_SPEED_MAX), color([random(200)]))
+    var speed = calculateSpeed(CELL_SPEED_MIN, CELL_SPEED_MAX, i);
+    createCell(i,random(width) , i, speed, color('black'))
+    createCell(i+'a', random(width) , i, speed, color('black'))
+    createCell(i+'b', random(width) , i, speed, color('black'))
+    createCell(i+'c', random(width) , i, speed, color('black'))
+    createCell(i+'d', random(width) , i, speed, color('black'))
+    createCell(i+'e', random(width) , i, speed, color('black'))
+    createJitterLines(i)
   }
 }
